dc1eb97a79a877740ef711e2646f08c2f157b355
Anna Navoznova <anna.navoznova@gmail.com>
2021-02-09 14:35:10 +0300

    Реализовано создание таблицы по массиву

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -5,30 +5,29 @@
  * Пример одного элемента, описывающего строку таблицы
  *
  *      {
-     *          name: '',
-     *          age: 25,
-     *          salary: '1000',
-     *          city: 'Petrozavodsk'
-     *   },
- *
- * @constructor
- */
-/**
- * Компонент, который реализует таблицу
- * с возможностью удаления строк
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
- *          name: 'Ilia',
- *          age: 25,
- *          salary: '1000',
- *          city: 'Petrozavodsk'
- *      },ы
+	 *          name: 'Ilia',
+	 *          age: 25,
+	 *          salary: '1000',
+	 *          city: 'Petrozavodsk'
+	 *   },
  *
  * @constructor
  */
 export default class UserTable {
-  constructor(rows) {
-  }
+	constructor(users) {
+		let trs = users.map(function (user) {
+			// TODO: переделать Object.values на columns
+			// const columns = ['name', 'age', 'salary', 'city'];
+			let userFieldValues = Object.values(user);
+			let tds = userFieldValues.map(x => `<td>${x}</td>`);
+			tds.push('<td><button>X</button></td>');
+			let tr = `<tr>${tds.join('')}</tr>`;
+			return tr;
+		});
+
+		let table = document.createElement('table');
+		table.innerHTML = trs.join('');
+
+		this.elem = table;
+	}
 }
