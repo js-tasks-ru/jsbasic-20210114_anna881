647abbed27d329403ba4e3990957c9f0a9f11248
Anna Navoznova <anna.navoznova@gmail.com>
2021-02-09 15:45:40 +0300

    Добавлен хидер таблицы

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,34 +14,39 @@
  * @constructor
  */
 export default class UserTable {
-
 	constructor(users) {
+		let table = document.createElement('table');
+
+		// хидер таблицы
+		let thead = document.createElement('thead');
+		let headers = ['Имя', 'Возраст', 'Зарплата', 'Город', ''];
+		let headerTr = '<tr>' + headers.map(item => `<th>${item}</th>`).join('') + '</tr>';
+		thead.innerHTML = headerTr;
+		table.appendChild(thead);		
+		
+		// тело таблицы
+		let tbody = document.createElement('tbody');
 		let trs = users.map(function (user) {
-			// TODO: переделать Object.values на columns
-			// const columns = ['name', 'age', 'salary', 'city'];
 			let userFieldValues = Object.values(user);
 			let tds = userFieldValues.map(x => `<td>${x}</td>`);
 			tds.push('<td><button>X</button></td>');
 			let tr = `<tr>${tds.join('')}</tr>`;
 			return tr;
 		});
-
-		let table = document.createElement('table');
-		table.innerHTML = trs.join('');
-
-		addEventToButtons()
-
-		function addEventToButtons() {
-			let buttons = table.querySelectorAll('button');
-			for (let button of buttons) {
-				button.addEventListener('click', deleteRowHandler);
-			}
-		}
-
+		
+		tbody.innerHTML = trs.join('');
+		table.appendChild(tbody);
+		
+		// вешаем хендлеры на кнопки "удалить"
 		function deleteRowHandler(event) {
 			let trToDelete = event.target.closest('tr');
 			trToDelete.remove();
 		};
+
+		let buttons = table.querySelectorAll('button');
+		for (let button of buttons) {
+			button.addEventListener('click', deleteRowHandler);
+		}
 
 		this.elem = table;
 	}
