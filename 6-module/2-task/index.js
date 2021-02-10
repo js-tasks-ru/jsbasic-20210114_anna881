06dbf29b6ef69a66af44cf37cb75ebba831be785
Anna Navoznova <anna.navoznova@gmail.com>
2021-02-10 12:26:24 +0300

    Создала карточку товара. Без кнопки Add

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,6 +1,25 @@
 import createElement from '../../assets/lib/create-element.js';
 
 export default class ProductCard {
-  constructor(product) {
-  }
+	constructor(product) {
+		let div = document.createElement('div');
+		div.classList.add('card');
+		div.innerHTML = `
+			<div class="card__top">
+				<img src="/assets/images/products/${product.image}" class="card__image" alt="product">
+				<span class="card__price">${getPriceStr(product.price)}</span>
+			</div>
+			<div class="card__body">
+				<div class="card__title">${product.name}</div>
+				<button type="button" class="card__button">
+					<img src="/assets/images/icons/plus-icon.svg" alt="icon">
+				</button>
+			</div>`;
+
+		function getPriceStr(price) {
+			return '€' + price.toFixed(2);
+		}
+
+		this.elem = div;
+	}
 }
