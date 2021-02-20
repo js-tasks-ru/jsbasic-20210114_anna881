b01659d25a9f21002f035e58d41882bf0d6a8282
Anna Navoznova <anna.navoznova@gmail.com>
2021-02-20 13:48:09 +0300

    Создала корневой элемент, добавала карточки товара на основе класса ProductCard

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -2,8 +2,22 @@ import createElement from '../../assets/lib/create-element.js';
 import ProductCard from '../../6-module/2-task/index.js';
 
 export default class ProductGrid {
-  constructor(products) {
-    this.products = products;
-    this.filters = {};
-  }
+	constructor(products) {
+		this.products = products;
+		this.filters = {};
+
+		let productsGrid = document.createElement('div');
+		this.elem = productsGrid;
+		productsGrid.classList.add('products-grid');
+		productsGrid.innerHTML = `<div class="products-grid__inner"></div>`;
+
+		products.forEach(product => {
+			let card = new ProductCard(product);
+			this.elem.querySelector('.products-grid__inner')
+				.append(card.elem);
+		});
+
+
+
+	}
 }
