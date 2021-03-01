c19c5543c55f6c575beab8b1a235ed1a33b3f0a2
Anna Navoznova <anna.navoznova@gmail.com>
2021-03-01 13:16:30 +0300

    Исправила ошибку в задаче 7-2 (добавление модального окна, метод close)

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -2,12 +2,9 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
 	constructor() {
-	}
-
-	open() {
-		const self = this;
-
-		const modalWindowHtml = `
+		this.modal = document.createElement('div');
+		this.modal.classList.add('Modal');
+		this.modalHtml = `
 			<div class="modal">
 				<div class="modal__overlay"></div>
 				<div class="modal__inner">
@@ -15,27 +12,28 @@ export default class Modal {
 						<button type="button" class="modal__close">
 						<img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
 						</button>
-						<h3 class="modal__title">${self.title}</h3>
+						<h3 class="modal__title">${this.title}</h3>
 					</div>
-					<div class="modal__body">${self.body}</div>
+					<div class="modal__body">${this.body}</div>
 				</div>
 			</div>`;
-
-		document.body.innerHTML = modalWindowHtml;
-		document.body.classList.add('is-modal-open');
-		self.modal = document.body.querySelector('.modal');
-
-		const closeButton = document.body.querySelector('.modal__close');
-		closeButton.addEventListener('click', () => self.close());
-
-		document.body.addEventListener('keydown', self.closeByEscape);
+		this.modal.innerHTML = this.modalHtml;
 	}
 
-	closeByEscape = (event) => {
+	open() {		
+		document.body.append(this.modal);
+		document.body.classList.add('is-modal-open');
+		
+		const closeButton = document.body.querySelector('.modal__close');
+		closeButton.addEventListener('click', () => this.close());
+
+		document.body.addEventListener('keydown', (event) => this.closeByEscape(event));
+	}
+
+	closeByEscape(event) {
 		if (event.code === 'Escape') {
 			this.close();
-		}
-		
+		}		
 		document.body.removeEventListener('keydown', this.closeByEscape);
 	}
 
