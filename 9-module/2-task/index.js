86b27dbae536c30fa70e97fca98b5ef04839a8c0
Anna Navoznova <anna.navoznova@gmail.com>
2021-03-05 17:09:36 +0300

    Создала базовые компоненты, взяла список товаров с сервера

--- 9-module/2-task/index.js
+++ 9-module/2-task/index.js
@@ -12,10 +12,38 @@ import Cart from '../../8-module/4-task/index.js';
 
 export default class Main {
 
-  constructor() {
-  }
+	constructor() {
+	}
 
-  async render() {
-    // ... ваш код
-  }
+	async render() {
+		let carousel = new Carousel(slides);
+		document.querySelector('[data-carousel-holder]').appendChild(carousel.elem);
+
+		let ribbonMenu = new RibbonMenu(categories);
+		document.querySelector('[data-ribbon-holder]').appendChild(ribbonMenu.elem);
+
+		let stepSlider = new StepSlider(5, 3);
+		document.querySelector('[data-slider-holder]').appendChild(stepSlider.elem);
+
+		let cartIcon = new CartIcon();
+		document.querySelector('[data-cart-icon-holder]').appendChild(cartIcon.elem);
+
+		let cart = new Cart(cartIcon);
+
+
+		//Получение списка товаров c сервера 		
+		let response = await fetch('products.json');
+		let products = await response.json();
+
+		let productsGrid = new ProductsGrid(products);
+		document.querySelector('[data-products-grid-holder]').innerHTML = '';
+		document.querySelector('[data-products-grid-holder]').appendChild(productsGrid.elem);
+
+		productsGrid.updateFilter({
+			noNuts: document.getElementById('nuts-checkbox').checked,
+			vegeterianOnly: document.getElementById('vegeterian-checkbox').checked,
+			maxSpiciness: stepSlider.value,
+			category: ribbonMenu.value
+		});
+	}
 }
