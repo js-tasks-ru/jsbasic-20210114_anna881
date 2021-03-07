be868dd8b209cbb0d59acad6a060c361eae0f6a9
Anna Navoznova <anna.navoznova@gmail.com>
2021-03-07 12:07:27 +0300

    Добавила пользовательские события на чекбоксы

--- 9-module/2-task/index.js
+++ 9-module/2-task/index.js
@@ -22,7 +22,9 @@ export default class Main {
 		let ribbonMenu = new RibbonMenu(categories);
 		document.querySelector('[data-ribbon-holder]').appendChild(ribbonMenu.elem);
 
-		let stepSlider = new StepSlider(5, 3);
+		let stepSlider = new StepSlider({
+			steps: 5
+		});
 		document.querySelector('[data-slider-holder]').appendChild(stepSlider.elem);
 
 		let cartIcon = new CartIcon();
@@ -44,6 +46,34 @@ export default class Main {
 			vegeterianOnly: document.getElementById('vegeterian-checkbox').checked,
 			maxSpiciness: stepSlider.value,
 			category: ribbonMenu.value
+		});
+
+		//Пользовательские события
+		document.body.addEventListener('product-add', (event) => {
+			let product = products.find(item => item.id === event.detail);
+			cart.addProduct(product);
+		});
+
+		stepSlider.elem.addEventListener('slider-change', (event) => {
+			productsGrid.updateFilter({
+				maxSpiciness: event.detail
+			});
+		});
+
+		ribbonMenu.elem.addEventListener('ribbon-select', (event) => {
+			productsGrid.updateFilter({
+				category: event.detail
+			});
+		});
+
+		let noNutsControl = document.querySelector('#nuts-checkbox');
+		noNutsControl.addEventListener('change', (event) => {
+			productsGrid.updateFilter({ noNuts: event.target.checked });
+		});
+
+		let vegetarianOnlyControl = document.querySelector('#vegeterian-checkbox');
+		vegetarianOnlyControl.addEventListener('change', (event) => {
+			productsGrid.updateFilter({ vegeterianOnly: event.target.checked });
 		});
 	}
 }
